b416818417ebfa887b4f00cb8dd0e55cbe8f0dc8
Dragomir Andrei <andrei.dragomir1401@stud.acs.upb.ro>
2023-05-12 00:52:50 +0300

    Update post.cpp

--- post.cpp
+++ post.cpp
@@ -10,46 +10,24 @@
 #include <string.h>
 #include <unistd.h>
 #include <iostream>
-#include "json.hpp"
-// for convenience
-using json = nlohmann::json;
 #include "headers.h"
 using namespace std;
 
-char *compute_post_request(char *host, char *url, char *content_type, char **keys, char **values,
-                           int body_data_fields_count, char **cookies, int cookies_count)
+char *compute_get_request(char *host, char *url, char *query_params,
+                          char **cookies, int cookies_count, char **tokens, int tokens_count)
 {
     char *message = (char *)calloc(BUFLEN, sizeof(char));
     char *line = (char *)calloc(LINELEN, sizeof(char));
-    char *body_data_buffer = (char *)calloc(LINELEN, sizeof(char));
 
-    // Write the method name, URL and protocol type
-    sprintf(line, "POST %s HTTP/1.1", url);
+    // Write the method name, URL, request params (if any) and protocol type
+    query_params != NULL ? sprintf(line, "GET %s?%s HTTP/1.1", url, query_params) : sprintf(line, "GET %s HTTP/1.1", url);
     compute_message(message, line);
 
     // Add the host
     sprintf(line, "Host: %s", host);
     compute_message(message, line);
 
-    // Add necessary headers (Content-Type and Content-Length are mandatory)
-    // in order to write Content-Length you must first compute the message size
-    sprintf(line, "Content-Type: %s", content_type);
-    compute_message(message, line);
-
-    json j;
-    for (int i = 0; i < body_data_fields_count; i++)
-    {
-        j[keys[i]] = values[i];
-    }
-
-    // Serialize the JSON
-    string s = j.dump();
-    strcpy(body_data_buffer, s.c_str());
-
-    snprintf(line, LINELEN, "Content-Length: %ld", strlen(body_data_buffer));
-    compute_message(message, line);
-
-    // Add cookies
+    // Add cookies, according to the protocol format
     if (cookies != NULL)
     {
         for (int i = 0; i < cookies_count; i++)
@@ -59,14 +37,19 @@ char *compute_post_request(char *host, char *url, char *content_type, char **key
         }
     }
 
-    // Add new line at end of header
+    // Add tokens, according to the protocol format
+    if (tokens != NULL)
+    {
+        for (int i = 0; i < tokens_count; i++)
+        {
+            snprintf(line, LINELEN, "Authorization: Bearer %s", tokens[i]);
+            compute_message(message, line);
+        }
+    }
+
+    // Add final new line
+    snprintf(line, LINELEN, "\n");
     compute_message(message, "");
 
-    // Add the actual payload data
-    memset(line, 0, LINELEN);
-    strcat(message, body_data_buffer);
-
-    free(line);
-    free(body_data_buffer);
     return message;
 }
