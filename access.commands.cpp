eb3242d19d78c9554ac0726878acb8fbee7f2288
Dragomir Andrei <andrei.dragomir1401@stud.acs.upb.ro>
2023-05-12 21:32:11 +0300

    need fine fixes

--- access.commands.cpp
+++ access.commands.cpp
@@ -9,7 +9,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include "headers.h"
+#include "headers.hpp"
 #include "json.hpp"
 using json = nlohmann::json;
 
@@ -67,13 +67,17 @@ void handle_register()
 
     // Deallocate the memory
     deallocate_memory(response, message, keys, values, host, url, content_type, ip, code);
-
-    // Close connection
-    close_connection(sockfd);
 }
 
 void handle_login(char *cookie)
 {
+    // Check if the user is already logged in
+    if (strcmp(cookie, "") != 0)
+    {
+        cout << "You are already logged in." << endl;
+        return;
+    }
+
     // Allocate memory for the array elements
     char *message = (char *)malloc(BUFLEN * sizeof(char));
     char *response = (char *)malloc(MAX_RESPONE_LEN * sizeof(char));
@@ -131,16 +135,13 @@ void handle_login(char *cookie)
 
     // Deallocate the memory
     deallocate_memory(response, message, keys, values, host, url, content_type, ip, code);
-
-    // Close connection
-    close_connection(sockfd);
 }
 
 void handle_enter_library(char *cookie, char *token)
 {
-    if (cookie == "")
+    if (!strcmp(cookie, ""))
     {
-        cout << "You must be logged in to enter the library." << endl;
+        cout << "You must be logged in to access the library." << endl;
         return;
     }
 
@@ -192,7 +193,71 @@ void handle_enter_library(char *cookie, char *token)
 
     // Deallocate the memory
     deallocate_memory1(response, message, host, url, ip, code);
+}
 
-    // Close connection
-    close_connection(sockfd);
+void handle_logout(char *cookie, char *token)
+{
+    if (!strcmp(cookie, ""))
+    {
+        cout << "You must be logged in to be able to log out" << endl;
+        return;
+    }
+
+    // Allocate memory for the array elements
+    char *message = (char *)malloc(BUFLEN * sizeof(char));
+    char *response = (char *)malloc(MAX_RESPONE_LEN * sizeof(char));
+
+    // Create the url, host and content type
+    char *host = new char[MAX_HOST_LEN];
+    char *url = new char[URL_SIZE];
+    strcpy(host, "34.254.242.81:8080");
+    strcpy(url, "/api/v1/tema/auth/logout");
+
+    // Create cookies and tokens
+    char *cookies[MAX_COOKIE_COUNT] = {cookie};
+    char *tokens[MAX_TOKEN_COUNT] = {token};
+
+    // Create the request
+    message = compute_get_request(host, url, NULL, cookies, 1, tokens, 1);
+
+    // Create the ip
+    char *ip = new char[MAX_IP_SIZE];
+    strcpy(ip, "34.254.242.81");
+
+    // Open connection
+    int sockfd = open_connection(ip, 8080, AF_INET, SOCK_STREAM, 0);
+
+    // Send the request to the server
+    send_to_server(sockfd, message);
+
+    // Get the response from the server
+    response = receive_from_server(sockfd);
+
+    // Extract HTTP code from response
+    char *code = new char[4];
+    extract_code(response, code);
+
+    // If response contains code 200, the user entered the library
+    char *res1 = strstr(response, "200 OK");
+    if (res1 != NULL)
+    {
+        cout << "Logout successful." << endl;
+    }
+
+    char *res = strstr(response, "400");
+    if (res != NULL)
+    {
+        cout << "You are not logged in or you dont have access to the library." << endl;
+    }
+    else if (res1 == NULL && res == NULL)
+    {
+        cout << "Other errors occured." << endl;
+    }
+
+    // Deallocate the memory
+    deallocate_memory1(response, message, host, url, ip, code);
+
+    // Invalidate token and cookie
+    strcpy(token, "");
+    strcpy(cookie, "");
 }
